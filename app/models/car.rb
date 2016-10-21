8df958701ce799bf48ba3457cbd166f593358792
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-10-21 15:17:21 -0700

    create view for user && validate car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,5 @@
 class Car < ApplicationRecord
+  validates :user_id, presence: true, uniqueness: true
+  validates :long, presence: true, numericality: { only_float: true }
+  validates :lat, presence: true, numericality: { only_float: true }
 end
