80da19796974dd9178e3afb2f19e3f96aa83f755
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-10-21 15:51:38 -0700

    add helper for computing distance

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -62,13 +62,14 @@ class CarsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_car
-      @car = Car.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def car_params
-      params.require(:car).permit(:user_id, :long, :lat)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def car_params
+    params.require(:car).permit(:user_id, :long, :lat)
+  end
 end
--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,2 +1,18 @@
 module CarsHelper
+  def dist (car_long, car_lat, officer_long, officer_lat)
+    # haversine
+    car_long_rad = car_long * Math::PI / 180
+    car_lat_rad = car_lat * Math::PI / 180
+    officer_long_rad = officer_long * Math::PI / 180
+    officer_lat_rad = officer_lat * Math::PI / 180
+
+    dlong = officer_long_rad - car_long_rad
+    dlat  = officer_lat_rad  - car_lat_rad
+
+    a = (Math.sin (dlong/2))**2 +
+        (Math.cos car_lat_rad) * (Math.cos officer_lat_rad) *
+        (Math.sin (dlat/2))**2
+
+    6_367_000 * 2 * Math.asin(Math.sqrt a)
+  end
 end
