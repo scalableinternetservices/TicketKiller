4703d1b7bd550c2993b903d8a09fb184e7eb49e0
Will Li <williamxli123@yahoo.com>
2016-10-22 01:05:01 -0700

    Improve static pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to Ticket Killer!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
