f0db1252bec287f7545b5b1fa498c5959beb8001
xiuluoxn <allenxn1992@gmail.com>
2016-11-18 15:41:00 -0800

    included the installation of ImageMagick

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'scalableinternetservices/demo/',
+      :bucket => 'scalableinternetservices/TicketKiller/',
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
