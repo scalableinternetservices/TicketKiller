09b454baaf65d248a8449f9df1500da658c2ca97
xiuluoxn <allenxn1992@gmail.com>
2016-11-17 16:36:19 -0800

    add marker for officers in the range

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,22 +1,25 @@
 module CarsHelper
   def self.distance(car, officer)
-    CarsHelper.dist_helper car.long, car.lat, officer.long, officer.lat
+    return CarsHelper.dist_helper car.long, car.lat, officer.long, officer.lat
   end
 
   def self.dist_helper(car_long, car_lat, officer_long, officer_lat)
-    # haversine
+    #haversine
     car_long_rad = car_long * Math::PI / 180
     car_lat_rad = car_lat * Math::PI / 180
     officer_long_rad = officer_long * Math::PI / 180
     officer_lat_rad = officer_lat * Math::PI / 180
 
-    dlong = officer_long_rad - car_long_rad
-    dlat  = officer_lat_rad  - car_lat_rad
-
-    a = (Math.sin (dlong/2))**2 +
-        (Math.cos car_lat_rad) * (Math.cos officer_lat_rad) *
-        (Math.sin (dlat/2))**2
-
-    6_367_000 * 2 * Math.asin(Math.sqrt a)
+    dLat = officer_lat_rad - car_lat_rad
+    dLon = officer_long_rad - car_long_rad
+    a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(car_lat_rad) * Math.cos(officer_lat_rad) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+     
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)) 
+    d = 6371 * c
+    return d;
   end
+  
 end
