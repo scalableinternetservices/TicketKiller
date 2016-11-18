bed9d6adafeeec8059662dd9d5d45809dcc4ebc9
xiuluoxn <allenxn1992@gmail.com>
2016-11-17 23:29:46 -0800

    Added destory user fuction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
